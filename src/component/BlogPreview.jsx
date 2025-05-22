60d5dddeec5820da31184440074d627073d3f7d7
Mubarak <adebowaleraji0103@gmail.com>
2025-05-22 17:15:27 +0100

    added

--- src/component/BlogPreview.jsx
+++ src/component/BlogPreview.jsx
@@ -6,7 +6,7 @@ function BlogPreview() {
         <span className="bg-purple-600  text-amber-200 absolute top-2 left-2 font-semibold">
           Lifestyle
         </span>
-        <p className="w-[200px] flex flex-col bg-white p-3 -top-1 relative flex justify-center left-1/2 text-center transform -translate-1/2">
+        <p className="w-[200px] flex flex-col bg-white p-3 -top-1 relative justify-center left-1/2 text-center transform -translate-1/2">
           Lorem ipsum dolor sit amet, consectetur.{" "}
           <span className="text-slate-400 text-[10px]">By shop owner</span>
         </p>
